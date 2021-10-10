9fb10d20cd1712f5e182bcd54c02f910a83f3f85
badie <gustavbadie@gmail.com>
2021-10-10 14:45:09 +0330

    code beautified

--- Account.java
+++ Account.java
@@ -4,79 +4,64 @@ package bank;
  *
  * @author hooman
  */
-class Account
-{
+class Account {
     private int id;
     private String name;
     private double balance;
     private String city;
-    
-    public Account (int id,String name, double balance,String city)
-    {
+
+    public Account(int id, String name, double balance, String city) {
         this.id = id;
-        this.name=name;
-        this.city=city;
-        if (balance>=0)
-        {
+        this.name = name;
+        this.city = city;
+        if (balance >= 0) {
             this.balance = balance;
-        }
-      	else
-        {
+        } else {
             throw new ArithmeticException();
-        }   
+        }
     }
-    
-    public int getID ()
-    {
+
+    public int getID() {
         return id;
     }
-	
-    public String getCity ()
-    {
+
+    public String getCity() {
         return city;
     }
-	
-    public void setCity(String city)
-    {
-            this.city=city;
+
+    public void setCity(String city) {
+        this.city = city;
     }
-    
-    public double getBalance ()
-    {
+
+    public double getBalance() {
         return balance;
     }
-  
-    boolean withdraw(double amount)
-    {
-        if (balance>=amount)
-        {
-            balance-=amount;
+
+    boolean withdraw(double amount) {
+        if (balance >= amount) {
+            balance -= amount;
             return true;
-        }
-        else
-        {
+        } else {
             return false;
-        } 
-    }  
-    boolean deposit(double amount)
-    {
-        if (amount>=0)
-        {
-            balance+=amount;
+        }
+    }
+
+    boolean deposit(double amount) {
+        if (amount >= 0) {
+            balance += amount;
             return true;
-        }
-        else
-        {
+        } else {
             return false;
-        }  
+        }
     }
-    public void print ()
-    {
-        //System.out.println ("ID: "+id +" Name: " +name+ " Balance: " + balance+ " City: "+ city); // old way
-	System.out.println(this);
+
+    public void print() {
+        // System.out.println ("ID: "+id +" Name: " +name+ " Balance: " + balance+ "
+        // City: "+ city); // old way
+        System.out.println(this);
     }
-    public String toString()
-    {
-            return "ID: "+id +" Name: " +name+ " Balance: " + balance+ " City: "+ city;
+
+    public String toString() {
+        return "ID: " + id + " Name: " + name + " Balance: " + balance + " City: " + city;
     }
 }
