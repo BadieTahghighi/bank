06362e996c560fa89a39394587786677798e9c9b
Armaghan <armaghanenayati@gmail.com>
2021-10-24 16:36:54 +0330

    hashmap added

---
+++ Bank/src/SearchStructure.java
@@ -0,0 +1,8 @@
+
+
+public abstract class SearchStructure<key,value> {
+    abstract public boolean insert(key key, value data);
+    abstract public boolean delete(key key);
+    abstract public value search(key key);
+    abstract public void print();
+}
